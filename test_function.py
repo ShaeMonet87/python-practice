32b9642d365b9ff565923b732dfe8c84a2912c9c
shaemonet87 <113361347+ShaeMonet87@users.noreply.github.com>
2026-08-12 13:56:47 +0100

    function updates

--- test_function.py
+++ test_function.py
@@ -16,3 +16,13 @@ def convertStringTempToCelcius(temp):
 
 temp_in_cel = convertStringTempToCelcius(currentTemp)
 print(temp_in_cel)
+
+def make_change(cents):
+    quarters = cents // 25
+    remaining_cents = cents - quarters * 25
+    dimes = remaining_cents // 10
+    remaining_cents = remaining_cents - dimes * 10
+    nickels = remaining_cents // 5
+    pennies = remaining_cents - nickels * 5
+    return quarters, dimes, nickels, pennies
+
